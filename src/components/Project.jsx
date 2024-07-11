9bca22a95aa7e71ecc7fd19912ed81b9e845bc37
Sahil1551 <106240812+Sahil1551@users.noreply.github.com>
2024-07-11 18:25:43 +0530

    assas

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,8 +8,13 @@ const Project = () => {
     <div id='project' className='Projects'>
       
       <h1 className='kanit-semibold'>Projects</h1>
+      <div className='ss'>
       <div className='card-container'>
       <Cards images={Mancot} title="Mancot" href="https://frontend-snowy-pi-75.vercel.app/" Description="Mancot is a street style brand developed using the MERN stack (MongoDB, Express.js, React, and Node.js). The website showcases a dynamic catalog, enabling customers to explore and purchase trendy apparel online. It features a user-friendly shopping cart, secure checkout, and an admin dashboard for managing products, orders, and customer interactions, ensuring a seamless and efficient shopping experience."/>
+      </div>
+      <div className='card-container'>
+      <Cards images={BlissBakerss} title="Bliss Bakerss" href="https://b-bfrontend.vercel.app/" Description="Bliss Bakerss is a delightful bakery founded by Ms. Divya Chhabra, offering a range of delicious cakes, cupcakes, and pastries. The website features an elegant catalog where customers can explore and order handcrafted treats. With a focus on quality and taste, Bliss Bakers ensures a wonderful online shopping experience."/>
+      </div>
       </div>
     </div>
   )
